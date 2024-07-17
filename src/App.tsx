9813f68d4fea6d86d4cca0332b149d6e179d67be
JoseA <joseantanaper@gmail.com>
2024-07-17 22:22:15 +0200

    Pixel Perfect Fit

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import '@style/base.scss'
 import Navbar from './components/layout/Navbar'
 import SSidebar from './components/layout/SSidebar'
 import ESidebar from './components/layout/ESidebar'
@@ -5,17 +6,21 @@ import Footer from './components/layout/Footer'
 const App = () => (
   <div className="app">
     <Navbar />
-    <SSidebar />
-    <ESidebar />
+    {/* <SSidebar />
+    <ESidebar /> */}
     <Footer />
-    <div className="container text-center">
+    <div className="app-main container-fluid text-center">
       <div className="row">
-        <div className="col">Column</div>
-        <div className="col">Column</div>
-        <div className="col">Column</div>
-      </div>
-      <div className="row" style={{ height: '1024px' }}>
-        <div className="col">Column</div>
+        <div
+          id="app-sidebar-start"
+          className="app-sidebar-start shadow col show"
+        >
+          SidebarStart
+        </div>
+        <div className="app-content col">Content</div>
+        <div id="app-sidebar-end" className="app-sidebar-end shadow col show">
+          SidebarEnd
+        </div>
       </div>
     </div>
   </div>
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,70 +1,113 @@
-const Navbar = () => (
-  <>
-    <nav className="navbar navbar-expand-lg sticky-top bg-body-tertiary">
-      <div className="container-fluid">
-        <button
-          className="btn btn-primary"
-          type="button"
-          data-bs-toggle="offcanvas"
-          data-bs-target="#offcanvasWithBothOptions"
-          aria-controls="offcanvasWithBothOptions"
-        >
-          =
-        </button>
-        Navbar
-        {/* <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarText"
-          aria-controls="navbarText"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button> */}
-        <div className="collapse navbar-collapse" id="navbarText">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Features
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Pricing
-              </a>
-            </li>
-          </ul>
-          <span className="navbar-text">
-            Navbar text with an inline element
-          </span>
+import React, { useEffect } from 'react'
+
+const Navbar = () => {
+  const toggleSidebarStart = () => {
+    console.log(
+      'toggleSidebar',
+      document.getElementById('app-sidebar-start')?.classList
+    )
+    if (
+      document.getElementById('app-sidebar-start')?.classList.contains('show')
+    ) {
+      document
+        .getElementById('app-sidebar-start')
+        ?.classList.replace('show', 'hide')
+    } else {
+      document
+        .getElementById('app-sidebar-start')
+        ?.classList.replace('hide', 'show')
+    }
+  }
+
+  const toggleSidebarEnd = () => {
+    console.log(
+      'toggleSidebarEnd',
+      document.getElementById('app-sidebar-end')?.classList
+    )
+    if (
+      document.getElementById('app-sidebar-end')?.classList.contains('show')
+    ) {
+      document
+        .getElementById('app-sidebar-end')
+        ?.classList.replace('show', 'hide')
+    } else {
+      document
+        .getElementById('app-sidebar-end')
+        ?.classList.replace('hide', 'show')
+    }
+  }
+
+  return (
+    <>
+      <nav className="navbar navbar-expand fixed-top bg-body-tertiary">
+        <div className="container-fluid">
+          <button
+            className="btn btn-danger"
+            type="button"
+            onClick={toggleSidebarStart}
+          >
+            =
+          </button>
+          <button
+            className="btn btn-primary"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasWithBothOptions"
+            aria-controls="offcanvasWithBothOptions"
+          >
+            =
+          </button>
+          Navbar: Pixel Perfect App
+          <div className="collapse navbar-collapse" id="navbarText">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Features
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Pricing
+                </a>
+              </li>
+            </ul>
+            <span className="navbar-text">
+              <button
+                className="btn btn-danger"
+                type="button"
+                onClick={toggleSidebarEnd}
+              >
+                =
+              </button>
+            </span>
+          </div>
+        </div>
+      </nav>
+      <div
+        className="offcanvas offcanvas-start shadow"
+        data-bs-scroll="true"
+        tabIndex={-1}
+        id="offcanvasWithBothOptions"
+        aria-labelledby="offcanvasWithBothOptionsLabel"
+      >
+        <div className="offcanvas-header">
+          <h5 className="offcanvas-title" id="offcanvasWithBothOptionsLabel">
+            Backdrop with scrolling
+          </h5>
+        </div>
+        <div className="offcanvas-body">
+          <p>
+            Try scrolling the rest of the page to see this option in action.
+          </p>
         </div>
       </div>
-    </nav>
-    <div
-      className="offcanvas offcanvas-start shadow"
-      style={{ top: '54px' }}
-      data-bs-scroll="true"
-      tabIndex={-1}
-      id="offcanvasWithBothOptions"
-      aria-labelledby="offcanvasWithBothOptionsLabel"
-    >
-      <div className="offcanvas-header">
-        <h5 className="offcanvas-title" id="offcanvasWithBothOptionsLabel">
-          Backdrop with scrolling
-        </h5>
-      </div>
-      <div className="offcanvas-body">
-        <p>Try scrolling the rest of the page to see this option in action.</p>
-      </div>
-    </div>
-  </>
-)
+    </>
+  )
+}
 
 export default Navbar
